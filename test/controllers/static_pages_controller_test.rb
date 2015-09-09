0ef2bea1655beba564e25e0e03a466a6f93e5787
Nishant Sharma <sharma.nishant159@gmail.com>
2015-09-09 12:10:39 +0000

    Static pages finished

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,22 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","help"
+    
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title","About"
   end
+  
+   
 
 end
