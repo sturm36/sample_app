593d33a092b5c4bced692792654296749e572edc
Nishant Sharma <sharma.nishant159@gmail.com>
2015-09-10 06:38:49 +0000

    improved test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+    
+    def setup
+       @base_title = "this is the ruby tutorial"
+    end
+    
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","home"
+    assert_select "title","home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","help"
+    assert_select "title","help | #{@base_title}"
     
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title","About"
+    assert_select "title","About | #{@base_title}"
   end
   
    
